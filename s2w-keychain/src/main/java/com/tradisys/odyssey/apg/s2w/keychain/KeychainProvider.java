c4e2dc260c27e7f888ed710c6a141286ac78671d
Pavel Sychykau <pavel@tradisys.com>
2019-04-13 17:54:27 +0200

    Add dummy keychain provider

---
+++ s2w-keychain/src/main/java/com/tradisys/odyssey/apg/s2w/keychain/KeychainProvider.java
@@ -0,0 +1,23 @@
+package com.tradisys.odyssey.apg.s2w.keychain;
+
+/**
+ * Common interface for keychain handling
+ */
+public interface KeychainProvider {
+
+    /**
+     * Perform seed generation
+     *
+     * @param seedName  name of a future seed to be stored in a keychain
+     * @return generated seed
+     */
+    String generateSeed(String seedName);
+
+    /**
+     * Find and return seed by its id
+     *
+     * @param id  id of a seed stored in a keychain
+     * @return  seed
+     */
+    String getSeedById(String id);
+}
