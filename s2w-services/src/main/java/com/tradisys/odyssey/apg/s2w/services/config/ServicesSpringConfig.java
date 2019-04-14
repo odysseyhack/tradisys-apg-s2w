1a9c1c87e7e7c451bad16b71439db5d1430a66f0
Alexandr Makoed <me@dsfn.gq>
2019-04-14 04:20:04 +0300

    Organization controller and service

--- s2w-services/src/main/java/com/tradisys/odyssey/apg/s2w/services/config/ServicesSpringConfig.java
+++ s2w-services/src/main/java/com/tradisys/odyssey/apg/s2w/services/config/ServicesSpringConfig.java
@@ -2,9 +2,11 @@ package com.tradisys.odyssey.apg.s2w.services.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tradisys.odyssey.apg.s2w.services.CustomerService;
+import com.tradisys.odyssey.apg.s2w.services.OrganizationService;
 import com.tradisys.odyssey.apg.s2w.services.RegistrationService;
 import com.tradisys.odyssey.apg.s2w.services.TasksService;
 import com.tradisys.odyssey.apg.s2w.services.impl.CustomerServiceImpl;
+import com.tradisys.odyssey.apg.s2w.services.impl.OrganizationServiceImpl;
 import com.tradisys.odyssey.apg.s2w.services.impl.RegistrationServiceImpl;
 import com.tradisys.odyssey.apg.s2w.services.impl.TaskServiceImpl;
 import org.springframework.context.annotation.Bean;
@@ -28,4 +30,7 @@ public class ServicesSpringConfig {
 
     @Bean
     public CustomerService customerService(){ return new CustomerServiceImpl(); }
+
+    @Bean
+    public OrganizationService organizationService(){ return new OrganizationServiceImpl(); }
 }
