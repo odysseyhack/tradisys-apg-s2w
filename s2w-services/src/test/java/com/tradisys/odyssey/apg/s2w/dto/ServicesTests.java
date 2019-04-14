987669bcda357a180d4b76b9a6bd9111ae8dd8d5
Pavel Sychykau <pavel@tradisys.com>
2019-04-14 07:15:55 +0200

    Tests

--- s2w-services/src/test/java/com/tradisys/odyssey/apg/s2w/dto/ServicesTests.java
+++ s2w-services/src/test/java/com/tradisys/odyssey/apg/s2w/dto/ServicesTests.java
@@ -7,6 +7,10 @@ import com.tradisys.odyssey.apg.s2w.services.OrganizationService;
 import com.tradisys.odyssey.apg.s2w.services.RegistrationService;
 import com.tradisys.odyssey.apg.s2w.services.TasksService;
 import com.tradisys.odyssey.apg.s2w.services.config.ServicesSpringConfig;
+import com.tradisys.odyssey.apg.s2w.services.impl.CustomerServiceImpl;
+import com.tradisys.odyssey.apg.s2w.services.impl.OrganizationServiceImpl;
+import com.tradisys.odyssey.apg.s2w.services.impl.RegistrationServiceImpl;
+import com.tradisys.odyssey.apg.s2w.services.impl.TaskServiceImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,15 +39,23 @@ public class ServicesTests {
     @Autowired
     private RegistrationService registrationService;
 
+
     @Test
     public void testCustomerService(){
         customerService = mock(CustomerService.class);
+        CustomerServiceImpl customerServiceImpl = mock(CustomerServiceImpl.class);
+        when(customerServiceImpl.findAllCustomers()).thenReturn(null);
+        assertNull(customerServiceImpl.findAllCustomers());
+
         when(customerService.ensureCustomerExists(1L)).thenReturn(true);
         assertTrue(customerService.ensureCustomerExists(1L));
     }
 
     @Test
     public void testTaskService(){
+        TaskServiceImpl taskServiceImpl = mock(TaskServiceImpl.class);
+        when(taskServiceImpl.getAllTasks()).thenReturn(null);
+        assertNull(taskServiceImpl.getAllTasks());
         tasksService = mock(TasksService.class);
         when(tasksService.getAllTasks()).thenReturn(null);
         assertNull(tasksService.getAllTasks());
@@ -52,12 +64,20 @@ public class ServicesTests {
     @Test
     public void testOrganizationService(){
         organizationService = mock(OrganizationService.class);
+        OrganizationServiceImpl organizationServiceImpl = mock(OrganizationServiceImpl.class);
+        when(organizationServiceImpl.findAllOrganizations()).thenReturn(null);
+        assertNull(organizationServiceImpl.findAllOrganizations());
+
         when(organizationService.ensureOrganizationExists(1L)).thenReturn(true);
         assertTrue(organizationService.ensureOrganizationExists(1L));
     }
 
     @Test
     public void testResistrationService(){
+        RegistrationServiceImpl regServiceImpl = mock(RegistrationServiceImpl.class);
+        when(regServiceImpl.register(null)).thenReturn(null);
+        assertNull(regServiceImpl.register(null));
+
         registrationService = mock(RegistrationService.class);
         when(registrationService.register(null)).thenReturn(null);
         assertNull(registrationService.register(null));
